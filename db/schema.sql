3ea1ddb1fe4804938ebd0eec97b46d0d274301c9
leena <annachandy123@gmail.com>
2021-11-09 16:21:22 +1100

    database and tables created

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,23 @@
-DROP DATABASE IF EXISTS   employee_db    ;
-CREATE DATABASE   employee_db            ;
-USE      employee_db            ;
-CREATE TABLE department  (
+DROP DATABASE IF EXISTS   employee_db;
+CREATE DATABASE   employee_db;
+USE  employee_db;
+CREATE TABLE department (
    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   dept_name VARCHAR(30) NOT NULL,
+   dept_name VARCHAR(30) 
 );
-CREATE TABLE role  (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE roles  (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 role_title VARCHAR(30),
-salary DECIMAL(10,2),
+salary DECIMAL,
 department_id INT,
-FOREIGN KEY(department_id)
-REFERENCES department(id)
+FOREIGN KEY(department_id) REFERENCES department(id)
 ON DELETE SET NULL
 );
-CREATE TABLE  employee   ( id INT NOT NULL AUTO_INCREMENT PRIMARY,
-first_name VARCHAR(30) NOT NULL,
-last_name VARCHAR(30) NOT NULL,
+CREATE TABLE  employee  ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) ,
+last_name VARCHAR(30) ,
 role_id INT,
 manager_id INT DEFAULT NULL,
 FOREIGN KEY (role_id)
-REFERENCES role(id) 
+REFERENCES roles(id) 
 ON DELETE SET NULL);
 
