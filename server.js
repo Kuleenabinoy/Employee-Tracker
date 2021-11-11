05966ff5b3d16058a66ca6e0b90f4e4aa089196d
leena <annachandy123@gmail.com>
2021-11-11 12:25:45 +1100

    view query added

--- server.js
+++ server.js
@@ -2,16 +2,102 @@ const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 
-const db = mysql.createConnection(
-    {
-        host: "localhost",
-        user: "root",
-        password: "rootroot",
-        database: "employee_db",
-    },
-    console.log("Connnected to employeedatabase")
-);
-db.query("SELECT * FROM department", function (err, results) {
-    if (err) throw err;
-    console.log(results);
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "rootroot",
+    database: "employee_db",
 });
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log("Connnected to employee database");
+    start();
+});
+function start() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View all Departments",
+                "View all Roles",
+                "View all Employees",
+                "Add Employee",
+                "Add Role",
+                "Add Department",
+                "Update Employee Role",
+                "Exit",
+            ],
+        })
+        .then((answer) => {
+            switch (answer.action) {
+                case "View all Departments":
+                    viewDepartment();
+                    break;
+                case "View all Employees":
+                    viewEmployee();
+                    break;
+                case "View all Roles":
+                    viewRoles();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Add Role":
+                    addRole();
+                    break;
+                case "Add Department":
+                    addDepartment();
+                    break;
+                case "Update Employee Role":
+                    updateEmployeerole();
+                    break;
+                case "Exit":
+                    exitPrompt();
+                    break;
+            }
+        });
+}
+function viewEmployee() {
+    let qry =
+        "SELECT employee.id,employee.first_name,employee.last_name, department.dept_name,roles.role_title,roles.salary,employee.manager_id  from roles JOIN employee on employee.role_id=roles.id JOIN department on department.id = roles.department_id ORDER BY employee.first_name ";
+    db.query(qry, function (err, results) {
+        if (err) throw err;
+        console.table(results);
+        start();
+    });
+}
+function viewRoles() {
+    let qry =
+        "SELECT roles.id AS RoleID,roles.role_title AS JOB_TITLE,department.dept_name AS Department_Name,roles.salary AS Salary from roles JOIN department ON roles.department_id=department.id";
+    db.query(qry, function (err, results) {
+        if (err) throw err;
+        console.table(results);
+        start();
+    });
+}
+function viewDepartment() {
+    let qry = "SELECT * FROM department";
+    db.query(qry, function (err, results) {
+        if (err) throw err;
+        console.table(results);
+        start();
+    });
+}
+function addEmployee() {
+    console.log("sucess");
+}
+function addRole() {
+    console.log("sucess");
+}
+function addDepartment() {
+    console.log("sucess");
+}
+function updateEmployeerole() {
+    console.log("sucess");
+}
+function exitPrompt() {
+    console.log("sucess");
+}
